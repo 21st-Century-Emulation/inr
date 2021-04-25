bcfb025ca6a361128f86ecf173b91f0407afd21e
David Tyler <david.tyler@metaswitch.com>
2021-04-25 13:12:59 +0100

    Added cpu id functionality across whole estate

--- src/inr.cr
+++ src/inr.cr
@@ -71,10 +71,10 @@ post "/api/v1/execute" do |env|
     set_flags(state.b, state.flags)
   when 0x34 then # INR (HL)
     address = (state.h.to_u16 << 8) | state.l.to_u16
-    get_request = Crest.get(READ_MEMORY_API, params: {:address => address.to_i})
+    get_request = Crest.get(READ_MEMORY_API, params: {:id => cpu.id, :address => address.to_i})
     value = get_request.body.to_u8 &+ 1
     set_flags(value, state.flags)
-    Crest.post(WRITE_MEMORY_API, params: {:address => address.to_i, :value => value.to_i})
+    Crest.post(WRITE_MEMORY_API, params: {:id => cpu.id, :address => address.to_i, :value => value.to_i})
     state.cycles &+= 5 # 5 additional cycles for memory access
   when 0x3C then # INR A
     state.a &+= 1
